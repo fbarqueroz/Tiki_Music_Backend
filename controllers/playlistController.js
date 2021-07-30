e904eeecdb57dd4d997fbe913950c5df1a356ce9
Fabian Barquero <fabianbarquero9@gmail.com>
2021-07-30 16:08:41 -0600

    Agrego cambios de playlist controller

--- controllers/playlistController.js
+++ controllers/playlistController.js
@@ -4,7 +4,7 @@ const playlistController = {};
 
 playlistController.create = async function (req, res, next) {
   try {
-    newPlaylist = await playlistService.createPlaylist(req.body);
+    const newPlaylist = await playlistService.createPlaylist(req.body);
     return res.status(201).json({ newPlaylist });
   } catch (error) {
     return res.status(400).json({ status: 400, message: error.message });
@@ -19,5 +19,32 @@ playlistController.getPlaylist = async function (req, res, next) {
     return res.status(400).json({ status: 400, message: error.message });
   }
 };
+
+playlistController.updatePlaylist = async function (req, res, next) {
+  try{
+      const newPlaylist = await playlistService.updatePlaylist(req.params, req.body);
+      return res.status(201).json({ newPlaylist });
+  }catch(error){
+      return res.status(400).json({ status: 400, message: error.message })
+  }
+}
+
+playlistController.deletePlaylist = async function (req, res, next) {
+  try{
+      const deletePlaylist = await playlistService.deletePlaylist(req.params);
+      return res.status(201).json({ deletePlaylist });
+  }catch(error){
+      return res.status(400).json({ status: 400, message: error.message })
+  }
+}
+
+playlistController.deletePlaylistBySong = async function (req, res, next) {
+  try{
+      const deleteSongs = await playlistService.deleteSongs(req.params, req.body);
+      return res.status(201).json({ deleteSongs });
+  }catch(error){
+      return res.status(400).json({ status: 400, message: error.message })
+  }
+}
 
 module.exports = playlistController;
