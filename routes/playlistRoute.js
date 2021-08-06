d41dc8184fbaa2daa56061df6ce0f0145108b34d
Fabian Barquero <fabianbarquero9@gmail.com>
2021-08-05 20:59:21 -0600

    Agrego cambios en route

--- routes/playlistRoute.js
+++ routes/playlistRoute.js
@@ -6,7 +6,7 @@ const playlistController = require('../controllers/playlistController');
 // Playlist routes
 router.post('/playlist', playlistController.create); // Create a new playlist
 router.get('/playlist', playlistController.getPlaylist); // Call all playlist
-router.get('/playlist/:id', playlistController.getPlaylist); // Call an specific playlist with the id
+router.get('/playlist/:id', playlistController.getPlaylistById); // Call an specific playlist with the id
 router.put('/playlist/:id',playlistController.updatePlaylist); // Add song into an specific playlist
 router.delete('/playlist-song/:id',playlistController.deletePlaylistBySong); // Delete a song from a specific playlist
 router.delete('/playlist/:id',playlistController.deletePlaylist); // Delete an specific playlist
