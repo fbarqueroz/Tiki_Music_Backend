bed4ca6e3ba3be6bb978deff5cd04e5a268a5ddb
Fabian Barquero <fabianbarquero9@gmail.com>
2021-08-05 20:51:22 -0600

    Agrego cambios en playlist service

--- controllers/playlistController.js
+++ controllers/playlistController.js
@@ -15,7 +15,8 @@ playlistController.create = async function (req, res, next) {
 // Get controller
 playlistController.getPlaylist = async function (req, res, next) {
   try {
-    const playlist = await playlistService.getPlaylist();
+    const id = req.query.id_user;
+    const playlist = await playlistService.getPlaylist({ id });
     return res.status(200).json({ status: 200, data: playlist, message: 'Successfully playlist retrieved' });
   } catch (error) {
     return res.status(400).json({ status: 400, message: error.message });
